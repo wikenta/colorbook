4d240d61f0b948971c21f1bbe4564bdd9fd1390f
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-04-28 23:34:46 +0500

    не в том порядке написала символы, которые надо экранировать

--- server/py_scripts/bot_commands/commands.py
+++ server/py_scripts/bot_commands/commands.py
@@ -60,7 +60,7 @@ def escape_markdown_v2(text: str) -> str:
     """
     Экранирует специальные символы для MarkdownV2
     """
-    special_chars = r"_*[]()~`>#+\-=|{}.!"
+    special_chars = r"\-_*[]()~`>#+=|{}.!"
     for char in special_chars:
         text = text.replace(char, f"\\{char}")
     return text
