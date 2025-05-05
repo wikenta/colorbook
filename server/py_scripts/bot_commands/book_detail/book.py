02bf2ede6e3831754824f820429e79d3c3689d55
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-05-06 00:06:21 +0500

    добавила корректную обработку выбора: отправлять новое сообщение или редактировать имеющееся

--- server/py_scripts/bot_commands/book_detail/book.py
+++ server/py_scripts/bot_commands/book_detail/book.py
@@ -6,6 +6,7 @@ from aiogram.types import FSInputFile
 from db_request.publisher import get_publisher_by_id
 from db_request.volume import get_book_by_id
 from db_request.cover_file import get_cover_files
+from ..sending import send_message, send_photo
 
 router = Router()
 logger = logging.getLogger("colorbook")
@@ -28,25 +29,21 @@ async def handle_book(callback_query: CallbackQuery):
         book_id = uuid.UUID(callback_query.data.removeprefix(PATH_BOOK))
     except ValueError:
         logger.error(f"Некорректный ID книги: {callback_query.data}")
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [BUTTON_MAIN]
-        ])
-        await callback_query.message.edit_text(
-            "Некорректный ID книги",
-            reply_markup=keyboard, 
-            parse_mode="HTML")
+        await send_message(
+            message=callback_query.message,
+            text="Некорректный ID книги",
+            buttons=[BUTTON_MAIN]
+        )
         return
 
     book = await get_book_by_id(book_id)
     if not book:
         logger.error(f"Книга не найдена: {book_id}")
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [BUTTON_MAIN]
-        ])
-        await callback_query.message.edit_text(
-            "Книга не найдена",
-            reply_markup=keyboard, 
-            parse_mode="HTML")
+        await send_message(
+            message=callback_query.message,
+            text="Книга не найдена",
+            buttons=[BUTTON_MAIN]
+        )
         return
     
     message = f"Книга: {book['full_name_ru']}\n\n"
@@ -55,13 +52,11 @@ async def handle_book(callback_query: CallbackQuery):
     publisher = await get_publisher_by_id(publisher_id)
     if not publisher:
         logger.error(f"Издатель не найден: {publisher_id}")
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [BUTTON_MAIN]
-        ])
-        await callback_query.message.edit_text(
-            message + "Издатель не найден",
-            reply_markup=keyboard, 
-            parse_mode="HTML")
+        await send_message(
+            message=callback_query.message,
+            text=message + "Издатель не найден",
+            buttons=[BUTTON_MAIN]
+        )
         return
     message += f"Издатель: {publisher['name_ru']}\n"
 
@@ -72,27 +67,16 @@ async def handle_book(callback_query: CallbackQuery):
     covers = await get_cover_files(book_id)
     if not covers:
         logger.error(f"Обложка не найдена: {book_id}")
-        await callback_query.message.edit_text(
-            message,
-            reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-                [button_publisher],
-                [BUTTON_MAIN]
-            ]),
-            parse_mode="HTML"
+        await send_message(
+            message=callback_query.message,
+            text=message,
+            buttons=[button_publisher, BUTTON_MAIN]
         )
         return
     
-    photo = FSInputFile('/colorbook/files/'+covers[0]['file_path'])
-    logger.info(f"Обложка найдена: {photo.path}")
-    await callback_query.message.delete()
-    logger.info(f"Сообщение удалено: {callback_query.message.message_id}")
-    await callback_query.message.answer_photo(
-        photo=photo,
-        caption=message,
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-            [button_publisher],
-            [BUTTON_MAIN]
-        ]),
-        parse_mode="HTML"
+    await send_photo(
+        message=callback_query.message,
+        path='/colorbook/files/'+covers[0]['file_path'],
+        text=message,
+        buttons=[button_publisher, BUTTON_MAIN]
     )
-    logger.info(f"Сообщение отправлено: {callback_query.message.message_id}")
--- server/py_scripts/bot_commands/book_detail/series.py
+++ server/py_scripts/bot_commands/book_detail/series.py
@@ -4,6 +4,7 @@ from aiogram.types import CallbackQuery
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from db_request.publisher import get_publisher_by_id
 from db_request.series import get_series_by_id
+from ..sending import send_message
 
 router = Router()
 logger = logging.getLogger("colorbook")
@@ -25,48 +26,39 @@ async def handle_series(callback_query: CallbackQuery):
     try:
         series_id = uuid.UUID(callback_query.data.removeprefix(PATH_SERIES))
     except ValueError:
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [BUTTON_MAIN]
-        ])
-        await callback_query.message.edit_text(
-            "Некорректный ID серии",
-            reply_markup=keyboard, 
-            parse_mode="HTML")
+        await send_message(
+            message=callback_query.message,
+            text="Некорректный ID серии",
+            buttons=[BUTTON_MAIN],
+        )
         return
 
     series = await get_series_by_id(series_id)
     if not series:
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [BUTTON_MAIN]
-        ])
-        await callback_query.message.edit_text(
-            "Серия не найдена",
-            reply_markup=keyboard, 
-            parse_mode="HTML")
+        await send_message(
+            message=callback_query.message,
+            text="Серия не найдена",
+            buttons=[BUTTON_MAIN],
+        )
         return
     
     message = f"Серия: {series['full_name_ru']}\n"
     publisher_id = series['publisher_id']
     publisher = await get_publisher_by_id(publisher_id)
     if not publisher:
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [BUTTON_MAIN]
-        ])
-        await callback_query.message.edit_text(
-            message + "Издатель не найден",
-            reply_markup=keyboard, 
-            parse_mode="HTML")
+        await send_message(
+            message=callback_query.message,
+            text="Издатель не найден",
+            buttons=[BUTTON_MAIN],
+        )
         return
     
     button_publisher = InlineKeyboardButton(
         text=publisher['name_ru'], 
         callback_data=PATH_PUBLISHER + str(publisher_id))
     message += f"Издатель: {publisher['name_ru']}\n"
-    await callback_query.message.edit_text(
-        message,
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-            [button_publisher],
-            [BUTTON_MAIN]
-        ]),
-        parse_mode="HTML"
+    await send_message(
+        message=callback_query.message,
+        text=message,
+        buttons=[button_publisher, BUTTON_MAIN],
     )
