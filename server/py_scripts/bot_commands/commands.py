577cefc58f33eb4ec66c5eb20556332025d23b55
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-05-06 13:48:00 +0500

    в main удалила тестирование кнопок

--- server/py_scripts/bot_commands/commands.py
+++ server/py_scripts/bot_commands/commands.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from .start import router as start_router
 from .books import router as books_router
-from .test import router as test_router
 from .book_detail.publisher import router as book_detail_publisher_router
 from .book_detail.series import router as book_detail_series_router
 from .book_detail.book import router as book_detail_book_router
@@ -10,7 +9,6 @@ from .book_detail.book import router as book_detail_book_router
 def register_routers(dp: Dispatcher):
     dp.include_router(start_router)
     dp.include_router(books_router)
-    dp.include_router(test_router)
     dp.include_router(book_detail_publisher_router)
     dp.include_router(book_detail_series_router)
     dp.include_router(book_detail_book_router)
@@ -19,7 +17,6 @@ async def set_commands(bot: Bot):
     commands = [
         BotCommand(command="start", description="Приветствие"),
         BotCommand(command="books", description="Показать список книг"),
-        BotCommand(command="book_detail", description="Показать подробную информацию о книге"),
-        BotCommand(command="test_buttons", description="Тест кнопок")
+        BotCommand(command="book_detail", description="Показать подробную информацию о книге")
     ]
     await bot.set_my_commands(commands)
