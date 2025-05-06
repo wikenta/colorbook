416c1c12d972a95165a520c3d9219442743697c0
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-05-06 13:13:30 +0500

    еще укоротила

--- server/py_scripts/bot_commands/test.py
+++ server/py_scripts/bot_commands/test.py
@@ -4,40 +4,19 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 router = Router()
 
 buttons = [
-    [InlineKeyboardButton(text="Всплывающее сообщение", callback_data="button1")],
-    [InlineKeyboardButton(text="Уведомление", callback_data="button2")],
-    [InlineKeyboardButton(text="Действие 1", callback_data="action:1")],
-    [InlineKeyboardButton(text="Действие 2", callback_data="action:2")]
+    InlineKeyboardButton(text="Действие 1", callback_data="action:1"),
+    InlineKeyboardButton(text="Действие 2", callback_data="action:2"),
+    InlineKeyboardButton(text="Уведомление", callback_data="button"),
 ]
 
-# Тестируем кнопки
 @router.message(F.text == "/test_buttons")
 async def test_buttons(message: Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    await message.reply("Выберите кнопку:", reply_markup=keyboard)
+    await message.reply("Нажмите кнопку:", reply_markup=keyboard)
 
-@router.callback_query(F.data == "button1")
-async def handle_button1(callback_query: CallbackQuery):
-    await callback_query.answer("Вы нажали кнопку 1!", show_alert=True)
-    # Вместо удаления клавиатуры - редактируем сообщение
-    await callback_query.message.edit_text(
-        "Вы выбрали Кнопку 1!\nХотите сделать что-то еще?",
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="Вернуться", callback_data="back_to_menu")]
-        ])
-    )
-
-@router.callback_query(F.data == "button2")
+@router.callback_query(F.data == "button")
 async def handle_button2(callback_query: CallbackQuery):
-    await callback_query.answer("Вы нажали кнопку 2!", show_alert=False)  # Всплывающее уведомление
-
-@router.callback_query(F.data == "back_to_menu")
-async def back_to_menu(callback_query: CallbackQuery):
-    # Возвращаем исходное меню
-    await callback_query.message.edit_text(
-        "Выберите кнопку:",
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=buttons)
-    )
+    await callback_query.answer("Вы получили уведомление!", show_alert=False)
 
 @router.callback_query(F.data.startswith("action:"))
 async def handle_action(callback: CallbackQuery):
