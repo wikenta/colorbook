c9e1c64482f833cb47d6f771e174a23b2165136c
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-08-24 20:12:22 +0500

    может я невовремя пытаюсь запросить переменные, что наиболее вероятно

--- services/admin-bot/bot_commands/load_image.py
+++ services/admin-bot/bot_commands/load_image.py
@@ -6,41 +6,42 @@ import os, logging
 
 router = Router()
 logger = logging.getLogger(__name__)
-ADMIN_ID = os.getenv(ADMIN_TG_ID)
-if ADMIN_ID is not None:
-    logger.info(f"Admin ID loaded: {ADMIN_ID}")
-else:
-    logger.error("ADMIN_TG_ID is not set in environment variables.")
-TG_BOT_ID = os.getenv(TELEGRAM_BOT_ID)
-if TG_BOT_ID is not None:
-    logger.info(f"Bot ID loaded: {TG_BOT_ID}")
-else:
-    logger.error("TELEGRAM_BOT_ID is not set in environment variables.")
-TG_BOT_LOGIN = os.getenv(TELEGRAM_BOT_LOGIN)
-if TG_BOT_LOGIN is not None:
-    logger.info(f"Bot login loaded: {TG_BOT_LOGIN}")
-else:
-    logger.error("TELEGRAM_BOT_LOGIN is not set in environment variables.")
-TG_API_TOKEN = os.getenv(TELEGRAM_API_TOKEN)
-if TG_API_TOKEN is not None:
-    logger.info("API token loaded successfully.")
-else:
-    logger.error("TELEGRAM_API_TOKEN is not set in environment variables.")
-DB_NAME = os.getenv(DB_NAME)
-if DB_NAME is not None:
-    logger.info(f"Database name loaded: {DB_NAME}")
-else:
-    logger.error("DB_NAME is not set in environment variables.")
-CLOUD_NAME = os.getenv(CLOUDINARY_CLOUD_NAME)
-if CLOUD_NAME is not None:
-    logger.info(f"Cloudinary cloud name loaded: {CLOUD_NAME}")
-else:
-    logger.error("CLOUDINARY_CLOUD_NAME is not set in environment variables.")
 
 #я получаю команду, прошу загрузить изображение
 #в ответ получаю изображение
 @router.message(F.text == "/load_image")
 async def cmd_load_image(message: Message):
+    ADMIN_ID = os.getenv(ADMIN_TG_ID)
+    if ADMIN_ID is not None:
+        logger.info(f"Admin ID loaded: {ADMIN_ID}")
+    else:
+        logger.error("ADMIN_TG_ID is not set in environment variables.")
+    TG_BOT_ID = os.getenv(TELEGRAM_BOT_ID)
+    if TG_BOT_ID is not None:
+        logger.info(f"Bot ID loaded: {TG_BOT_ID}")
+    else:
+        logger.error("TELEGRAM_BOT_ID is not set in environment variables.")
+    TG_BOT_LOGIN = os.getenv(TELEGRAM_BOT_LOGIN)
+    if TG_BOT_LOGIN is not None:
+        logger.info(f"Bot login loaded: {TG_BOT_LOGIN}")
+    else:
+        logger.error("TELEGRAM_BOT_LOGIN is not set in environment variables.")
+    TG_API_TOKEN = os.getenv(TELEGRAM_API_TOKEN)
+    if TG_API_TOKEN is not None:
+        logger.info("API token loaded successfully.")
+    else:
+        logger.error("TELEGRAM_API_TOKEN is not set in environment variables.")
+    DB_NAME = os.getenv(DB_NAME)
+    if DB_NAME is not None:
+        logger.info(f"Database name loaded: {DB_NAME}")
+    else:
+        logger.error("DB_NAME is not set in environment variables.")
+    CLOUD_NAME = os.getenv(CLOUDINARY_CLOUD_NAME)
+    if CLOUD_NAME is not None:
+        logger.info(f"Cloudinary cloud name loaded: {CLOUD_NAME}")
+    else:
+        logger.error("CLOUDINARY_CLOUD_NAME is not set in environment variables.")
+
     if str(message.from_user.id) != ADMIN_ID:
         await message.reply(
             "Здесь можно настраивать бот, но вы не администратор."
