4f0f9678a2860f954569bbb05a07f0612cf08121
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-08-24 19:21:50 +0500

    добавила ограничение: отвечает только мне

--- services/admin-bot/bot_commands/load_image.py
+++ services/admin-bot/bot_commands/load_image.py
@@ -1,12 +1,23 @@
 from aiogram import F, Router
 from aiogram.types import Message
 from tools.sending import send_message
+from tools.loading import ADMIN_TG_ID
+import os
+
 router = Router()
+ADMIN_ID = os.getenv(ADMIN_TG_ID)
 
 #я получаю команду, прошу загрузить изображение
 #в ответ получаю изображение
 @router.message(F.text == "/load_image")
 async def cmd_load_image(message: Message):
+    if message.from_user.id != ADMIN_ID:
+        await message.reply(
+            "Здесь можно настраивать бот, но вы не администратор."
+            "Воспользоваться ботом можно здесь:\n"
+            "@color_book_bot"
+        )
+        return
     text = (
         "Загрузите изображение\n\n" +
         "Кстати, ваш id: " + str(message.from_user.id)
@@ -16,6 +27,8 @@ async def cmd_load_image(message: Message):
 
 @router.message(F.photo)
 async def handle_photo(message: Message):
+    if message.from_user.id != ADMIN_ID:
+        return
     #присылаю это же фото в ответ
     await message.answer_photo(photo=message.photo[-1].file_id, caption="Вот ваше изображение!")
     await message.reply("В какой том хотите добавить изображение?")
