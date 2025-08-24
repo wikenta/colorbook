99cd3de37822144f3de2e3adefc84b8f752983ad
Dinara Aldarova <dinara_aldarova@mail.ru>
2025-08-24 20:14:35 +0500

    получается лучше запрашивать id в момент, когда получаю сообщение

--- services/admin-bot/bot_commands/load_image.py
+++ services/admin-bot/bot_commands/load_image.py
@@ -16,40 +16,12 @@ async def cmd_load_image(message: Message):
         logger.info(f"Admin ID loaded: {ADMIN_ID}")
     else:
         logger.error("ADMIN_TG_ID is not set in environment variables.")
-    TG_BOT_ID = os.getenv(TELEGRAM_BOT_ID)
-    if TG_BOT_ID is not None:
-        logger.info(f"Bot ID loaded: {TG_BOT_ID}")
-    else:
-        logger.error("TELEGRAM_BOT_ID is not set in environment variables.")
-    TG_BOT_LOGIN = os.getenv(TELEGRAM_BOT_LOGIN)
-    if TG_BOT_LOGIN is not None:
-        logger.info(f"Bot login loaded: {TG_BOT_LOGIN}")
-    else:
-        logger.error("TELEGRAM_BOT_LOGIN is not set in environment variables.")
-    TG_API_TOKEN = os.getenv(TELEGRAM_API_TOKEN)
-    if TG_API_TOKEN is not None:
-        logger.info("API token loaded successfully.")
-    else:
-        logger.error("TELEGRAM_API_TOKEN is not set in environment variables.")
-    DB_NAME = os.getenv(DB_NAME)
-    if DB_NAME is not None:
-        logger.info(f"Database name loaded: {DB_NAME}")
-    else:
-        logger.error("DB_NAME is not set in environment variables.")
-    CLOUD_NAME = os.getenv(CLOUDINARY_CLOUD_NAME)
-    if CLOUD_NAME is not None:
-        logger.info(f"Cloudinary cloud name loaded: {CLOUD_NAME}")
-    else:
-        logger.error("CLOUDINARY_CLOUD_NAME is not set in environment variables.")
-
     if str(message.from_user.id) != ADMIN_ID:
         await message.reply(
             "Здесь можно настраивать бот, но вы не администратор."
             "Воспользоваться ботом можно здесь:\n"
             "@color_book_bot"
         )
-        logger.warning(f"Unauthorized access attempt by user ID: {message.from_user.id}, "+
-                       f"admin ID: {ADMIN_ID}")
         return
     text = (
         "Загрузите изображение\n\n"
@@ -60,7 +32,7 @@ async def cmd_load_image(message: Message):
 
 @router.message(F.photo)
 async def handle_photo(message: Message):
-    if message.from_user.id != ADMIN_ID:
+    if message.from_user.id != os.getenv(ADMIN_TG_ID):
         return
     #присылаю это же фото в ответ
     await message.answer_photo(photo=message.photo[-1].file_id, caption="Вот ваше изображение!")
